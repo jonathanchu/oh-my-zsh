155d05e431266aa77d7af98a02401a28a7437ccb
Jonathan Chu <jonathan.chu@me.com>
2011-05-31 21:52:33 -0400

    Add my own aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -23,3 +23,20 @@ alias sl=ls # often screw this up
 alias afind='ack-grep -il'
 
 alias x=extract
+
+# my custom aliases
+alias la='ls -la'
+alias l='ls -CF'
+alias rm='rm -i'
+alias mv='mv -i'
+alias mkdir='mkdir -p'
+alias du='du -kh'
+alias df='df -kh'
+alias cpwd='pwd|xargs echo -n|pbcopy'
+alias delpyc="find . -name '*.pyc' -delete"
+alias flushdns="dscacheutil -flushcache"
+alias conf='mate /etc/hosts /Users/jonathan/.bashrc /Users/jonathan/.bash_profile /Users/jonathan/.gitconfig /etc/apache2/httpd.conf /etc/apache2/users/jonathan.conf'
+alias apachectl='sudo apachectl'
+alias rs='python manage.py runserver'
+alias delmongolock="rm /usr/local/var/mongodb/mongod.lock"
+alias recoverdbs="rm /usr/local/var/mongodb/mongod.lock /usr/local/var/postgres/postmaster.pid"
